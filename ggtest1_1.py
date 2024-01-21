dc5891372c19959c7a2f220f8d5060afc14151ec
jiwonseo1212 <jiwonseo@beringlab.com>
2024-01-22 00:53:38 +0900

    add sol2

--- ggtest1_1.py
+++ ggtest1_1.py
@@ -2,13 +2,15 @@
 import random
 from ggtest1_1_sols.sol1 import solution
 import numpy as np
+from ggtest1_1_sols.sol2 import solution2
 
 # Define the mean and standard deviation of your normal distribution
-mean = 50
+mean = 300
 std_dev = 15
 
 random_ints = [int(round(np.random.normal(mean, std_dev))) for i in range(10000000)]
 print(random_ints[:10])
 print(solution(random_ints, 10))
+print(solution2(random_ints, 10))
 
 
---
+++ ggtest1_1_sols/sol2.py
@@ -0,0 +1,34 @@
+"""
+-- Python cases --
+
+Write a function called solution(data, n) that takes in a list of less than 100 integers and a number n, and returns that same list but with all of the numbers that occur more than n times removed entirely. The returned list should retain the same ordering as the original list - you don't want to mix up those carefully-planned shift rotations! For instance, if data was [5, 10, 15, 10, 7] and n was 1, solution(data, n) would return the list [5, 15, 7] because 10 occurs twice, and thus was removed from the list entirely.   
+
+Input:
+solution.solution([1, 2, 2, 3, 3, 3, 4, 5, 5], 1)
+Output:
+    1,4
+"""
+
+from collections import defaultdict
+from utils import algorithm_performance_decorator
+
+
+@algorithm_performance_decorator
+def solution2(data, n):
+    integer_dict = defaultdict(int)
+
+    for num in data:
+        integer_dict[num] +=1 
+
+    new_list = []
+    remove_set = set([k for k, v in integer_dict.items() if v > n])
+    data = data[::-1]
+    while data:
+        num = data.pop()
+        if num not in remove_set:
+            new_list.append(num)
+
+    return new_list
+
+
+
