939f606f55d6de121f8aab2972beb0a2796f5acc
jiwonseo1212 <jiwonseo1212@beringlab.com>
2024-01-23 21:47:47 +0900

    sol3 added, level 1-1 completed

--- ggtest1_1.py
+++ ggtest1_1.py
@@ -3,6 +3,7 @@ import random
 from ggtest1_1_sols.sol1 import solution
 import numpy as np
 from ggtest1_1_sols.sol2 import solution2
+from ggtest1_1_sols.sol3 import solution2 as solution3
 
 # Define the mean and standard deviation of your normal distribution
 mean = 300
@@ -12,5 +13,7 @@ random_ints = [int(round(np.random.normal(mean, std_dev))) for i in range(100000
 print(random_ints[:10])
 print(solution(random_ints, 10))
 print(solution2(random_ints, 10))
+print(solution3(random_ints, 10))
+
 
 
